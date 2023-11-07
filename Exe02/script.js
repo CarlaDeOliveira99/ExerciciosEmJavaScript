09221c00752243417dfc42a8aeda8058f9da1cf0
Carla de Oliveira <carlaolive999@gmail.com>
2023-11-07 15:25:40 -0300

    correção das cores, incluido anuciado da questão e adicionado gif do exe2

--- Exe02/script.js
+++ Exe02/script.js
@@ -1,6 +1,4 @@
 /*Criar uma tela com um botão, quando apertar botão criar uma div na tela (pode incher a tela de divs, sugiro usar um scroll) */
-let btnClicado = document.getElementById('btnDiv')
-
 
 document.getElementById('btnDiv').addEventListener("click",function() {
 
