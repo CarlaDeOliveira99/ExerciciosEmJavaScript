d9b699fb178db69c575dee405ed17cc9f5c2dc9b
Carla de Oliveira <carlaolive999@gmail.com>
2023-11-08 18:31:16 -0300

    correção no código

--- Exe03/script.js
+++ Exe03/script.js
@@ -1,15 +1,3 @@
-/*3 - Faça um programa que “pegue” uma lista <ol> no HTML e coloque negrito dinamicamente a partir de um nome digitado no seu input. Por exemplo
-A sua tela possui uma lista de nomes:
-João
-Claudio
-Marcio
-Usando um input você digita “Jo”, então o programa no mesmo momento deverá devolver o seguinte resultado:
-João (negrito)
-Claudio
-Marcio
- */
-
-
 areaPesquisa.addEventListener("keyup", () => {
     let areaPesquisa = document.getElementById('areaPesquisa')
     let  lista = document.getElementsByTagName('ol')
@@ -23,14 +11,12 @@ areaPesquisa.addEventListener("keyup", () => {
         let valorDigitado = areaPesquisa.value.toLowerCase().trim()
 
         if (valorLista.includes(valorDigitado)) {
+            element.style.fontSize = "17pt"
             element.style.fontWeight = "bold"
         }else{
             element.style.fontWeight = "normal"
         }
-    })
-
-
-   
+    })   
 });
 
 
